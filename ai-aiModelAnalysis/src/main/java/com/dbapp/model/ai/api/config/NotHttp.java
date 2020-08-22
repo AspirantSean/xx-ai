1ae0aff60ff298d0ba8eb194bb0b1a040c96afee
谢思恩 <shawn.xie@dbappsecurity.com.cn>
2020-08-22 12:21:37 +0800

    feat: 剔除无用的类

--- ai-aiModelAnalysis/src/main/java/com/dbapp/model/ai/api/config/NotHttp.java
+++
@@ -1,9 +0,0 @@
-package com.dbapp.model.ai.api.config;
-
-import java.lang.annotation.*;
-
-@Target({ElementType.PARAMETER})
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface NotHttp {
-}
